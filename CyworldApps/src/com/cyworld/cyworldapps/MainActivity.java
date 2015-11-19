1d51366f08f38718a651c0ab086ae16ef3f79a02
kotaro <skouta310@gmail.com>
2015-11-19 19:10:38 +0900

    RoomList更新

--- CyworldApps/src/com/cyworld/cyworldapps/MainActivity.java
+++ CyworldApps/src/com/cyworld/cyworldapps/MainActivity.java
@@ -1,11 +1,15 @@
 package com.cyworld.cyworldapps;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends Activity {
@@ -15,35 +19,24 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		ListView listview = (ListView)findViewById(R.id.roomList);
-		ArrayList<String> list = new ArrayList<String>();
-		RoomListAdapter roomAdapter = new RoomListAdapter(MainActivity.this);
+		ListView listview = (ListView)findViewById(android.R.id.list);
+		List<String> list = new ArrayList<>();
+        Log.d("list","挿入");
 
-		//testData いらなくなったら消す
-		list.add("RoomName");
-		list.add("RoomName");
-		list.add("RoomName");
 
-		roomAdapter.setList(list);
-		listview.setAdapter(roomAdapter);
-	}
+//		//testData いらなくなったら消す
+		list.add("a");
+		list.add("b");
+		list.add("c");
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+//        String[] list = {"aaa"};
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+        RoomListAdapter adapter = new RoomListAdapter(MainActivity.this,list);
+        Log.d("list","set");
+//		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1,list);
+		listview.setAdapter(adapter);
+        Log.d("adapter", "set");
+
+
+    }
 }
--- CyworldApps/src/com/cyworld/cyworldapps/RoomListAdapter.java
+++ CyworldApps/src/com/cyworld/cyworldapps/RoomListAdapter.java
@@ -1,58 +1,73 @@
 package com.cyworld.cyworldapps;
 
-import java.util.ArrayList;
-
-import android.annotation.SuppressLint;
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Adapter;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
-@SuppressLint("ViewHolder")
-public class RoomListAdapter extends BaseAdapter{
+import java.util.ArrayList;
+import java.util.List;
 
-	Context context;
-	LayoutInflater layoutinflater = null;
-	ArrayList<String> data;
+/**
+ * Created by kotaro on 15/11/19.
+ */
+public class RoomListAdapter extends BaseAdapter {
+    Context context;
+    LayoutInflater layoutinflater;
+    List<String> data;
 
-	public RoomListAdapter(Context context) {
-		this.context = context;
-		this.layoutinflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-	}
+    public RoomListAdapter(Context context,List<String> list) {
+        Log.d("constractor", "constractor");
+        this.context = context;
+        this.data = list;
+        layoutinflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
 
-	//list取得
-	public void setList(ArrayList<String> data){
-		this.data = data;
-	}
+    @Override
+    public int getCount() {
+        // TODO 自動生成されたメソッド・スタブ
+        Log.d("get","getCount");
+        return 0;
+    }
 
-	@Override
-	public int getCount() {
-		// TODO 自動生成されたメソッド・スタブ
-		return 0;
-	}
+    @Override
+    public String getItem(int position) {
+        // TODO 自動生成されたメソッド・スタブ
+        Log.d("get","getItem");
+        return null;
+    }
 
-	@Override
-	public Object getItem(int position) {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
-	}
 
-	@Override
-	public long getItemId(int position) {
-		// TODO 自動生成されたメソッド・スタブ
-		return 0;
-	}
+    @Override
+    public long getItemId(int position) {
+        // TODO 自動生成されたメソッド・スタブ
+        Log.d("get", "getItemId");
+        return 0;
+    }
 
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		convertView = layoutinflater.inflate(R.layout.list_layout,parent,false);
-//		TextView roomName = (TextView)convertView.findViewById(R.id.roomName);
-//		TextView roomCom = (TextView)convertView.findViewById(R.id.roomCom);
-//		roomName.setText(data.get(position));
-//		roomCom.setText("Comment...");
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
 
-		return convertView;
-	}
+        Log.d("View", "inflate");
+        View v = convertView;
+
+        v = layoutinflater.inflate(R.layout.list_layout, parent,true);
+        Log.d("View","convertView");
+
+        TextView roomName = (TextView)v.findViewById(R.id.roomName);
+        TextView roomCom = (TextView)v.findViewById(R.id.roomCom);
+
+        roomName.setText(data.get(position));
+        Log.d("data",data.get(position));
+        roomCom.setText("Comment...");
+
+        return v;
+    }
 
 }
+
